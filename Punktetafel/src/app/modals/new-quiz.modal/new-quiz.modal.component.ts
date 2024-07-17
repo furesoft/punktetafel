f89d0ebafe02c87560939f19b7d4a5071fee3f76
Chris <filmee24@gmail.com>
2024-07-17 09:54:37 +0000

    refactor: Cleanup

--- Punktetafel/src/app/modals/new-quiz.modal/new-quiz.modal.component.ts
+++ Punktetafel/src/app/modals/new-quiz.modal/new-quiz.modal.component.ts
@@ -2,30 +2,30 @@ import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Quiz } from '../../../models/quiz.model';
 import  * as short from "short-uuid";
+import { Category } from '../../../models/category.model';
 
 @Component({
   selector: 'app-new-quiz',
   templateUrl: './new-quiz.modal.component.html',
-  styleUrl: './new-quiz.modal.component.scss'
+  styleUrl: './new-quiz.modal.component.scss',
 })
 export class NewQuizModalComponent {
   quiz: Quiz = {
     id: short.generate().substring(0, 6),
-    title: "",
+    title: '',
     questions: [],
-    categories: []
+    categories: [],
   };
   newQuiz: boolean = true;
+  categories: Category[] = [];
 
   constructor(public activeModal: NgbActiveModal) {}
 
-  submit()
-  {
+  submit() {
     this.activeModal.close(this.quiz);
   }
 
-  addCategory()
-  {
-    this.quiz.categories.push("");
+  addCategory() {
+    this.categories.push({name: ""});
   }
 }
